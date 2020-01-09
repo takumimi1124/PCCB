788ec8b9eea25d63bec43e4b1e1a7fea17160572
TakumiMasuda <taku@TakuminoAir.local>
2020-01-10 01:44:33 +0900

    dfsの実装

---
+++ some_sum.py
@@ -0,0 +1,24 @@
+def dfs(i, sum):
+  # n個決め終わったら、今まで和のsumがkと等しいかを示す
+  if i == n: return sum == k
+
+  # a[i]を使わない場合
+  if dfs(i + 1, sum ): return True
+
+  # a[i]を使う場合
+  if dfs(i + 1, sum + a[i]): return True
+
+  # a[i]を使う使わないに拘らずkが作られないのでFalseを返す
+  return False
+
+
+########################################
+
+n = int(input())
+a = list(map(int, input().split()))
+k = int(input())
+
+if dfs(0, 0):
+  print("Yes")
+else:
+  print("No")
