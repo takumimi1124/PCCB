0475daf9ba467e441dd032230fd5ac464da3cbbf
TakumiMasuda <taku@TakuminoAir.local>
2020-01-20 00:51:47 +0900

    up

---
+++ Chap_2/Geekly/coin.py
@@ -0,0 +1,16 @@
+V = int(input())
+
+change = 1000 - V
+c = 0
+
+while change > 0:
+  if change >= 500: change -= 500
+  elif change >= 100: change -= 100
+  elif change >= 50: change -= 50
+  elif change >= 10: change -= 10
+  elif change >= 5: change -= 5
+  else: change -= 1
+
+  c += 1
+
+print(c)
